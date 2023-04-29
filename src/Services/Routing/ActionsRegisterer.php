a578e161793de6fd9602a6ba0dfc23dfabe2bedc
mortazavi <s.mohammad027@live.com>
2023-04-29 15:30:00 +0330

    adding extra parameters feature to an action implemented;

--- src/Services/Routing/ActionsRegisterer.php
+++ src/Services/Routing/ActionsRegisterer.php
@@ -1,72 +1,85 @@
 <?php
 
-    namespace Hans\Valravn\Services\Routing;
+	namespace Hans\Valravn\Services\Routing;
 
-    use Illuminate\Routing\Router;
-    use Illuminate\Support\Str;
+	use Illuminate\Routing\Router;
+	use Illuminate\Support\Str;
 
-    class ActionsRegisterer {
-        private Router $router;
-        protected bool $withId = false;
-        private array $methods = [ 'GET', 'POST', 'PATCH', 'DELETE' ];
+	class ActionsRegisterer {
+		private Router $router;
+		protected bool $withId = false;
+		protected array $parameters = [];
+		private array $methods = [ 'GET', 'POST', 'PATCH', 'DELETE' ];
 
-        public function __construct( protected string $name, protected string $controller, protected string $prefix ) {
-            $this->router = app( Router::class );
-        }
+		public function __construct( protected string $name, protected string $controller, protected string $prefix ) {
+			$this->router = app( Router::class );
+		}
 
-        public function withId(): self {
-            $this->withId = true;
+		public function withId(): self {
+			$this->withId = true;
 
-            return $this;
-        }
+			return $this;
+		}
 
-        public function get( string $action ): void {
-            $this->addRoute( 'get', $action );
-        }
+		public function parameters( ...$parameters ): self {
+			$this->parameters = $parameters;
 
-        public function post( string $action ): void {
-            $this->addRoute( 'post', $action );
-        }
+			return $this;
+		}
 
-        public function patch( string $action ): void {
-            $this->addRoute( 'patch', $action );
-        }
+		public function get( string $action ): void {
+			$this->addRoute( 'get', $action );
+		}
 
-        public function delete( string $action ): void {
-            $this->addRoute( 'delete', $action );
-        }
+		public function post( string $action ): void {
+			$this->addRoute( 'post', $action );
+		}
 
-        protected function addRoute( string $method, string $action ) {
-            $this->registerRoute( $this->makeUri( $action ), Str::of( $method )->upper(), Str::camel( $action ) );
-        }
+		public function patch( string $action ): void {
+			$this->addRoute( 'patch', $action );
+		}
 
-        protected function registerRoute( string $uri, string $method, string $action ): void {
-            if ( in_array( $method, $this->methods ) ) {
-                $this->router->addRoute( $method, $uri, [ $this->controller, $action ] )
-                             ->name( "$this->name.{$this->getRouteNamePrefix()}.$action" );
-            }
-        }
+		public function delete( string $action ): void {
+			$this->addRoute( 'delete', $action );
+		}
 
-        protected function addIdParameter( string $uri ): string {
-            return trim( $uri, '/' ) . "/{" . $this->name . "}";
-        }
+		protected function addRoute( string $method, string $action ) {
+			$this->registerRoute( $this->makeUri( $action ), Str::of( $method )->upper(), Str::camel( $action ) );
+		}
 
-        protected function addIdParameterWhen( bool $condition, string $uri ): string {
-            if ( $condition ) {
-                $uri = $this->addIdParameter( $uri );
-            }
+		protected function registerRoute( string $uri, string $method, string $action ): void {
+			if ( in_array( $method, $this->methods ) ) {
+				$this->router->addRoute( $method, $uri, [ $this->controller, $action ] )
+				             ->name( "$this->name.{$this->getRouteNamePrefix()}.$action" );
+			}
+		}
 
-            return $uri;
-        }
+		protected function addIdParameter( string $uri ): string {
+			return trim( $uri, '/' ) . "/{" . $this->name . "}";
+		}
 
-        protected function makeUri( string $action ): string {
-            $uri = "/$this->name/$this->prefix";
-            $uri = $this->addIdParameterWhen( $this->withId, $uri );
+		protected function addIdParameterWhen( bool $condition, string $uri ): string {
+			if ( $condition ) {
+				$uri = $this->addIdParameter( $uri );
+			}
 
-            return Str::of( $action )->snake()->replace( '_', '-' )->prepend( "$uri/" );
-        }
+			return $uri;
+		}
 
-        protected function getRouteNamePrefix(): string {
-            return 'actions';
-        }
-    }
+		protected function makeUri( string $action ): string {
+			$uri = "/$this->name/$this->prefix";
+			$uri = $this->addIdParameterWhen( $this->withId, $uri );
+
+			foreach ( $this->parameters as $parameter ) {
+				$uri .= is_array( $parameter ) ?
+					"/" . key( $parameter ) . "/{" . current( $parameter ) . "}" :
+					"/{" . $parameter . "}";
+			}
+
+			return Str::of( $action )->snake()->replace( '_', '-' )->prepend( "$uri/" );
+		}
+
+		protected function getRouteNamePrefix(): string {
+			return 'actions';
+		}
+	}
