901cf793f1b7c170906945e559f0454c64159b1b
mortazavi <s.mohammad027@live.com>
2023-04-29 19:06:48 +0330

    Router Facades updated;

--- src/Facades/Router.php
+++ src/Facades/Router.php
@@ -6,6 +6,7 @@
 	use Illuminate\Support\Facades\Facade;
 
 	/**
+	 * @method static RoutingService  name( string $name )
 	 * @method static RoutingService resource( string $name, string $controller )
 	 * @method static RoutingService relations( string $controller, callable $func )
 	 * @method static RoutingService actions( string $controller, callable $func, string $prefix = '-actions' )
@@ -14,6 +15,13 @@
 	 * @see RoutingService
 	 */
 	class Router extends Facade {
+
+		/**
+		 * Indicates if the resolved instance should be cached.
+		 *
+		 * @var bool
+		 */
+		protected static $cached = false;
 
 		protected static function getFacadeAccessor() {
 			return 'routing-service';
