2dd2a0d07dc44dabd01c3b1fe3537fe5523740a5
mortazavi <s.mohammad027@live.com>
2023-05-01 11:19:12 +0330

    ControllersCommandTest completed;

--- src/Commands/Controllers.php
+++ src/Commands/Controllers.php
@@ -1,52 +1,48 @@
 <?php
 
-    namespace Hans\Valravn\Commands;
+	namespace Hans\Valravn\Commands;
 
-    use Illuminate\Console\Command;
-    use Illuminate\Support\Facades\Artisan;
-    use Throwable;
+	use Illuminate\Console\Command;
+	use Illuminate\Support\Facades\Artisan;
+	use Throwable;
 
-    class Controllers extends Command {
-        /**
-         * The name and signature of the console command.
-         *
-         * @var string
-         */
-        protected $signature = 'valravn:controllers {name} {--namespace=} {--v=v1} {--req|requests} {--res|resources}';
+	class Controllers extends Command {
+		/**
+		 * The name and signature of the console command.
+		 *
+		 * @var string
+		 */
+		protected $signature = 'valravn:controllers {namespace} {name} {--v=1} {--req|requests} {--res|resources}';
 
-        /**
-         * The console command description.
-         *
-         * @var string
-         */
-        protected $description = 'creates controller classes.';
+		/**
+		 * The console command description.
+		 *
+		 * @var string
+		 */
+		protected $description = 'creates controller classes.';
 
-        /**
-         * Execute the console command.
-         *
-         * @return void
-         * @throws Throwable
-         */
-        public function handle() {
-            $name      = $this->argument( 'name' );
-            $namespace = $this->option( 'namespace' );
-            $version   = $this->option( 'v' );
+		/**
+		 * Execute the console command.
+		 *
+		 * @return void
+		 * @throws Throwable
+		 */
+		public function handle() {
+			$name      = $this->argument( 'name' );
+			$namespace = $this->argument( 'namespace' );
+			$version   = 'v' . filter_var( $this->option( 'v' ), FILTER_SANITIZE_NUMBER_INT );
 
-            if ( ! $namespace ) {
-                $this->error( 'namespace is required!' );
-            }
+			Artisan::call( "valravn:controller", [
+				'name'        => $name,
+				'--namespace' => $namespace,
+				'--v'         => $version,
+				'--relations' => true,
+				'--actions'   => true,
+				'--resources' => $this->option( 'resources' ),
+				'--requests'  => $this->option( 'requests' ),
+			] );
 
-            Artisan::call( "valravn:controller", [
-                'name'        => $name,
-                '--namespace' => $namespace,
-                '--v'         => $version,
-                '--relations' => true,
-                '--actions'   => true,
-                '--resources' => $this->option( 'resources' ),
-                '--requests'  => $this->option( 'requests' ),
-            ] );
+			$this->info( "controller classes successfully created!" );
+		}
 
-            $this->info( "controller classes successfully created!" );
-        }
-
-    }
+	}
---
+++ tests/Feature/Commands/ControllersCommandTest.php
@@ -0,0 +1,123 @@
+<?php
+
+	namespace Hans\Tests\Valravn\Feature\Commands;
+
+	use Hans\Tests\Valravn\TestCase;
+	use Illuminate\Support\Facades\File;
+
+	class ControllersCommandTest extends TestCase {
+
+		/**
+		 * @test
+		 *
+		 * @return void
+		 */
+		public function NotEnoughArguments(): void {
+			$this->expectExceptionMessage( 'Not enough arguments (missing: "name").' );
+			$this->artisan( 'valravn:controllers' );
+		}
+
+		/**
+		 * @test
+		 *
+		 * @return void
+		 */
+		public function namespaceIsRequired(): void {
+			$this->expectExceptionMessage( 'Not enough arguments (missing: "namespace").' );
+			$this->artisan( 'valravn:controllers posts' );
+		}
+
+		/**
+		 * @test
+		 *
+		 * @return void
+		 */
+		public function controllers(): void {
+			$crud      = app_path( "Http/Controllers/V1/Blog/Post/PostCrudController.php" );
+			$relations = app_path( "Http/Controllers/V1/Blog/Post/PostRelationsController.php" );
+			$actions   = app_path( "Http/Controllers/V1/Blog/Post/PostActionsController.php" );
+			File::delete( [ $crud, $relations, $actions ] );
+			self::assertFileDoesNotExist( $crud );
+			self::assertFileDoesNotExist( $relations );
+			self::assertFileDoesNotExist( $actions );
+
+			$command = $this->artisan( 'valravn:controllers blog posts' );
+			$command->execute();
+			$command->expectsOutput( "controller classes successfully created!" );
+
+			self::assertFileExists( $crud );
+			self::assertFileExists( $relations );
+			self::assertFileExists( $actions );
+		}
+
+
+		/**
+		 * @test
+		 *
+		 * @return void
+		 */
+		public function requests(): void {
+			$store  = app_path( "Http/Requests/V1/Blog/Post/PostStoreRequest.php" );
+			$update = app_path( "Http/Requests/V1/Blog/Post/PostUpdateRequest.php" );
+			File::delete( [ $store, $update ] );
+			self::assertFileDoesNotExist( $store );
+			self::assertFileDoesNotExist( $update );
+
+			$command = $this->artisan( 'valravn:controllers blog posts --requests' );
+			$command->execute();
+			$command->expectsOutput( "controller classes successfully created!" );
+
+			self::assertFileExists( $store );
+			self::assertFileExists( $update );
+		}
+
+		/**
+		 * @test
+		 *
+		 * @return void
+		 */
+		public function resources(): void {
+			$resource   = app_path( "Http/Resources/V1/Blog/Post/PostResource.php" );
+			$collection = app_path( "Http/Resources/V1/Blog/Post/PostCollection.php" );
+			File::delete( [ $resource, $collection ] );
+			self::assertFileDoesNotExist( $resource );
+			self::assertFileDoesNotExist( $collection );
+
+			$command = $this->artisan( 'valravn:controllers blog posts --resources' );
+			$command->execute();
+			$command->expectsOutput( "controller classes successfully created!" );
+
+			self::assertFileExists( $resource );
+			self::assertFileExists( $collection );
+		}
+
+		/**
+		 * @test
+		 *
+		 * @return void
+		 */
+		public function version(): void {
+			$crud       = app_path( "Http/Controllers/V2/Blog/Post/PostCrudController.php" );
+			$relations  = app_path( "Http/Controllers/V2/Blog/Post/PostRelationsController.php" );
+			$actions    = app_path( "Http/Controllers/V2/Blog/Post/PostActionsController.php" );
+			$store      = app_path( "Http/Requests/V2/Blog/Post/PostStoreRequest.php" );
+			$update     = app_path( "Http/Requests/V2/Blog/Post/PostUpdateRequest.php" );
+			$resource   = app_path( "Http/Resources/V2/Blog/Post/PostResource.php" );
+			$collection = app_path( "Http/Resources/V2/Blog/Post/PostCollection.php" );
+			File::delete( [ $crud, $relations, $actions, $store, $update, $resource, $collection ] );
+
+			$command = $this->artisan( 'valravn:controllers blog posts --requests --resources --v=2' );
+			$command->execute();
+			$command->expectsOutput( "controller classes successfully created!" );
+
+			self::assertFileExists( $crud );
+			self::assertFileExists( $relations );
+			self::assertFileExists( $actions );
+			self::assertFileExists( $store );
+			self::assertFileExists( $update );
+			self::assertFileExists( $resource );
+			self::assertFileExists( $collection );
+		}
+
+
+	}
