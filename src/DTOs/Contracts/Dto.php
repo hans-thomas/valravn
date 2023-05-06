cdc781937bec237bed98b5b87861d94875bef0c3
mortazavi <s.mohammad027@live.com>
2023-05-06 11:40:20 +0330

    Dto::makeFromArray accepts a keyName;

--- src/DTOs/Contracts/Dto.php
+++ src/DTOs/Contracts/Dto.php
@@ -1,42 +1,42 @@
 <?php
 
-    namespace Hans\Valravn\DTOs\Contracts;
+	namespace Hans\Valravn\DTOs\Contracts;
 
-    use Illuminate\Support\Collection;
+	use Illuminate\Support\Collection;
 
-    abstract class Dto {
-        protected Collection $data;
+	abstract class Dto {
+		protected Collection $data;
 
-        public function __construct( array $data ) {
-            $this->data = $this->parse( $data );
-        }
+		public function __construct( array $data ) {
+			$this->data = $this->parse( $data );
+		}
 
-        abstract protected function parse( array $data ): Collection;
+		abstract protected function parse( array $data ): Collection;
 
-        public static function make( array $data ): static {
-            return new static( $data );
-        }
+		public static function make( array $data ): static {
+			return new static( $data );
+		}
 
-        public static function makeFromArray( array|Collection $data ): static {
-            $output = null;
-            $data   = $data instanceof Collection ? $data->toArray() : $data;
+		public static function makeFromArray( array|Collection $data, string $keyName = 'related' ): static {
+			$output = null;
+			$data   = $data instanceof Collection ? $data->toArray() : $data;
 
-            foreach ( $data as $index => $value ) {
-                if ( is_int( $value ) ) {
-                    $output[ 'related' ][] = [ 'id' => $value ];
-                } elseif ( is_array( $value ) ) {
-                    $output[ 'related' ][] = [ 'id' => $index, 'pivot' => $value ];
-                }
-            }
+			foreach ( $data as $index => $value ) {
+				if ( is_int( $value ) ) {
+					$output[ $keyName ][] = [ 'id' => $value ];
+				} elseif ( is_array( $value ) ) {
+					$output[ $keyName ][] = [ 'id' => $index, 'pivot' => $value ];
+				}
+			}
 
-            return new static( $output );
-        }
+			return new static( $output );
+		}
 
-        public static function export( array $data ): Collection {
-            return ( new static( $data ) )->getData();
-        }
+		public static function export( array $data ): Collection {
+			return ( new static( $data ) )->getData();
+		}
 
-        public function getData(): Collection {
-            return $this->data;
-        }
-    }
+		public function getData(): Collection {
+			return $this->data;
+		}
+	}
