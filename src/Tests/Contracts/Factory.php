335ca52a88dc706e83953bd01022f1a72bbf9e1c
mortazavi <s.mohammad027@live.com>
2023-05-09 13:12:24 +0330

    tests commented;

--- src/Tests/Contracts/Factory.php
+++ src/Tests/Contracts/Factory.php
@@ -8,32 +8,81 @@
 	use Illuminate\Support\Collection;
 
 	abstract class Factory {
+
+		/**
+		 * Store the created model
+		 *
+		 * @var Model
+		 */
 		protected Model $model;
 
 		public function __construct( array $data = [] ) {
 			$this->model = static::factory()->create( $data );
 		}
 
+		/**
+		 * Return related factory instance
+		 *
+		 * @return EloquentFactory
+		 */
 		abstract protected static function getFactory(): EloquentFactory;
 
+		/**
+		 * Return related repository instance
+		 *
+		 * @return Repository
+		 */
 		abstract public static function getRepository(): Repository;
 
+		/**
+		 * PreCreate hook executes before factory ran
+		 *
+		 * @return void
+		 */
 		protected static function preCreateHook(): void { }
 
+		/**
+		 * Execute hooks and then factory
+		 *
+		 * @return EloquentFactory
+		 */
 		protected static function factory(): EloquentFactory {
 			static::preCreateHook();
 
 			return static::getFactory();
 		}
 
+		/**
+		 * Create an instance statically
+		 *
+		 * @param array $data
+		 *
+		 * @return static
+		 */
 		public static function create( array $data = [] ): static {
 			return new static( $data );
 		}
 
+		/**
+		 * Create an instance statically but don't store
+		 *
+		 * @param int|null $count
+		 * @param array    $data
+		 *
+		 * @return Collection|Model
+		 */
 		public static function make( ?int $count = null, array $data = [] ): Collection|Model {
 			return static::factory()->count( $count )->make( $data );
 		}
 
+		/**
+		 * Create many fake data at once
+		 *
+		 * @param int   $count
+		 * @param array $data
+		 *
+		 * @return Collection
+		 */
 		public static function createMany( int $count = 10, array $data = [] ): Collection {
 			return static::factory()
 			             ->count( $count )
@@ -41,6 +90,11 @@
 			             ->map( fn( Model $model ) => $model->fresh() );
 		}
 
+		/**
+		 * Return related model
+		 *
+		 * @return Model
+		 */
 		public function getModel(): Model {
 			return $this->model->refresh();
 		}
--- src/Tests/TestCase.php
+++ src/Tests/TestCase.php
@@ -12,22 +12,68 @@
 	abstract class TestCase extends BaseTestCase {
 		use CreatesApplication, RefreshDatabase, WithFaker;
 
+		/**
+		 * Make a get json request and set the token
+		 *
+		 * @param             $uri
+		 * @param array       $headers
+		 * @param string|null $token
+		 *
+		 * @return TestResponse
+		 */
 		public function getJsonRequest( $uri, array $headers = [], string $token = null ): TestResponse {
 			return $this->getJson( $uri, $headers + [ 'Authorization' => $token ] );
 		}
 
+		/**
+		 * Make a post json request and set the token
+		 *
+		 * @param             $uri
+		 * @param array       $data
+		 * @param array       $headers
+		 * @param string|null $token
+		 *
+		 * @return TestResponse
+		 */
 		public function postJsonRequest( $uri, array $data = [], array $headers = [], string $token = null ): TestResponse {
 			return $this->postJson( $uri, $data, $headers + [ 'Authorization' => $token ] );
 		}
 
+		/**
+		 * Make a patch json request and set the token
+		 *
+		 * @param             $uri
+		 * @param array       $data
+		 * @param array       $headers
+		 * @param string|null $token
+		 *
+		 * @return TestResponse
+		 */
 		public function patchJsonRequest( $uri, array $data = [], array $headers = [], string $token = null ): TestResponse {
 			return $this->patchJson( $uri, $data, $headers + [ 'Authorization' => $token ] );
 		}
 
+		/**
+		 * Make a delete json request and set the token
+		 *
+		 * @param             $uri
+		 * @param array       $data
+		 * @param array       $headers
+		 * @param string|null $token
+		 *
+		 * @return TestResponse
+		 */
 		public function deleteJsonRequest( $uri, array $data = [], array $headers = [], string $token = null ): TestResponse {
 			return $this->deleteJson( $uri, $data, $headers + [ 'Authorization' => $token ] );
 		}
 
+		/**
+		 * Convert resource class to an array
+		 *
+		 * @param BaseJsonResource $resource
+		 *
+		 * @return array
+		 */
 		public function resourceToJson( BaseJsonResource $resource ): array {
 			return json_decode(
 				$resource->toResponse( request() )->content(),
