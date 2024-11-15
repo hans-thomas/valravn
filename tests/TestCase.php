47543f520f0d36293515f770166bc04085a09b88
Mohammad Mortazavi <s.mohammad027@live.com>
2024-11-15 16:21:17 +0330

    Fix migrating issue;

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -22,7 +22,6 @@ class TestCase extends BaseTestCase
     protected function setUp(): void
     {
         parent::setUp();
-        $this->loadMigrationsFrom(__DIR__.'/Core/migrations');
     }
 
     /**
@@ -49,18 +48,6 @@ class TestCase extends BaseTestCase
         return [
             ValravnServiceProvider::class,
         ];
-    }
-
-    /**
-     * Override application aliases.
-     *
-     * @param Application $app
-     *
-     * @return array
-     */
-    protected function getPackageAliases($app)
-    {
-        return [/* 'Acme' => 'Acme\Facade' */];
     }
 
     /**
@@ -105,6 +92,18 @@ class TestCase extends BaseTestCase
         $router->get(
             '/queries/posts',
             fn () => PostCollection::make(Post::all())->parseQueries()
+        );
+    }
+
+    /**
+     * Define database migrations.
+     *
+     * @return void
+     */
+    protected function defineDatabaseMigrations(): void
+    {
+        $this->loadMigrationsFrom(
+            __DIR__.'/Core/migrations'
         );
     }
 
