da96d5e2ed3ab50ff4844725ae2d50e60ffc37d3
Mohammad Mortazavi <s.mohammad027@live.com>
2024-12-30 17:54:08 +0330

    Remove depricated makeAbility method;

--- src/Policies/Traits/PolicyHelperTrait.php
+++ src/Policies/Traits/PolicyHelperTrait.php
@@ -26,16 +26,4 @@ trait PolicyHelperTrait
         return array_reverse(explode('\\', strtolower($model)))[1].'-'.
                array_reverse(explode('\\', strtolower($model)))[0];
     }
-
-    /**
-     * Guess the ability.
-     *
-     * @return string
-     *
-     * @deprecated use guessAbility() instead
-     */
-    public function makeAbility(): string
-    {
-        return $this->normalizeModelName($this->getModel()).'-'.debug_backtrace()[1]['function'];
-    }
 }
