54c427953f6011b5fe3536b49da9e2c4071de4b6
Mohammad Mortazavi <s.mohammad027@live.com>
2025-01-11 11:15:49 +0330

    Add tests to cover actions;

---
+++ tests/Feature/Services/Includes/LimitActionTest.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Hans\Valravn\Tests\Feature\Services\Includes;
+
+use Hans\Valravn\Http\Resources\Contracts\VJsonResource;
+use Hans\Valravn\Services\Includes\Actions\LimitAction;
+use Hans\Valravn\Services\Includes\Actions\OrderAction;
+use Hans\Valravn\Services\Includes\Actions\SelectAction;
+use Hans\Valravn\Services\Includes\IncludingService;
+use Hans\Valravn\Tests\Core\Factories\CategoryFactory;
+use Hans\Valravn\Tests\Core\Factories\CommentFactory;
+use Hans\Valravn\Tests\Core\Factories\PostFactory;
+use Hans\Valravn\Tests\Core\Resources\Category\CategoryCollection;
+use Hans\Valravn\Tests\Core\Resources\Post\PostResource;
+use Hans\Valravn\Tests\Instances\Http\Includes\CategoriesIncludes;
+use Hans\Valravn\Tests\Instances\Http\Includes\CommentsIncludes;
+use Hans\Valravn\Tests\TestCase;
+use Illuminate\Support\Collection;
+use PHPUnit\Framework\Attributes\Test;
+
+class LimitActionTest extends TestCase
+{
+    private Collection $posts;
+    private IncludingService $service;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->posts = PostFactory::new()
+                                     ->count(3)
+                                     ->has(CategoryFactory::new()->count(5))
+                                     ->create();
+        $resource = PostResource::make($this->posts->first());
+        $this->service = app(IncludingService::class, ['resource' => $resource]);
+    }
+
+    #[Test]
+    public function apply(): void
+    {
+        $model = $this->posts->first();
+        $data = $this->service->registerIncludesUsingQueryString('categories:limit(2)')
+                      ->applyRequestedIncludes($model)
+                      ->getIncludedData();
+
+        self::assertEquals(
+            [
+                'categories' => CategoryCollection::make($model->categories()->take(2)->get())
+            ],
+            $data
+        );
+    }
+
+    #[Test]
+    public function applyWithNoParam(): void
+    {
+        $model = $this->posts->first();
+        $data = $this->service->registerIncludesUsingQueryString('categories:limit()')
+                      ->applyRequestedIncludes($model)
+                      ->getIncludedData();
+
+        self::assertEquals(
+            [
+                'categories' => CategoryCollection::make($model->categories)
+            ],
+            $data
+        );
+    }
+}
---
+++ tests/Feature/Services/Includes/SelectActionTest.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Hans\Valravn\Tests\Feature\Services\Includes;
+
+use Hans\Valravn\Services\Includes\IncludingService;
+use Hans\Valravn\Tests\Core\Factories\CategoryFactory;
+use Hans\Valravn\Tests\Core\Factories\PostFactory;
+use Hans\Valravn\Tests\Core\Resources\Category\CategoryCollection;
+use Hans\Valravn\Tests\Core\Resources\Post\PostResource;
+use Hans\Valravn\Tests\TestCase;
+use Illuminate\Support\Collection;
+use PHPUnit\Framework\Attributes\Test;
+
+class SelectActionTest extends TestCase
+{
+    private Collection $posts;
+    private IncludingService $service;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->posts = PostFactory::new()
+                                     ->count(3)
+                                     ->has(CategoryFactory::new()->count(5))
+                                     ->create();
+        $resource = PostResource::make($this->posts->first());
+        $this->service = app(IncludingService::class, ['resource' => $resource]);
+    }
+
+    #[Test]
+    public function apply(): void
+    {
+        $model = $this->posts->first();
+        $data = $this->service->registerIncludesUsingQueryString('categories:select(id)')
+                      ->applyRequestedIncludes($model)
+                      ->getIncludedData();
+
+        self::assertEquals(
+            [
+                'categories' => CategoryCollection::make($model->categories()->select('id')->get()),
+            ],
+            $data
+        );
+    }
+
+    #[Test]
+    public function applyWithNoParam(): void
+    {
+        $model = $this->posts->first();
+        $data = $this->service->registerIncludesUsingQueryString('categories:select()')
+                      ->applyRequestedIncludes($model)
+                      ->getIncludedData();
+
+        self::assertEquals(
+            [
+                'categories' => CategoryCollection::make($model->categories)
+            ],
+            $data
+        );
+    }
+}
