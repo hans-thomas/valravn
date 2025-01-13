03da725eead6e5c5ae6cd20cf3dfe2a06dae8425
Mohammad Mortazavi <s.mohammad027@live.com>
2025-01-13 22:09:31 +0330

    Add tests to cover Policy section;

--- tests/Feature/Policies/VPolicyTest.php
+++ tests/Feature/Policies/VPolicyTest.php
@@ -8,9 +8,12 @@ use Hans\Valravn\Tests\Instances\Policies\PostPolicy;
 use Hans\Valravn\Tests\TestCase;
 use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Auth\Access\Response;
+use Illuminate\Foundation\Auth\User;
 use Illuminate\Support\Facades\Gate;
+use Mockery;
 use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\Attributes\Test;
+use ReflectionClass;
 
 class VPolicyTest extends TestCase
 {
@@ -20,6 +23,13 @@ class VPolicyTest extends TestCase
 
         $this->actingAs(UserFactory::new()->create());
         Gate::policy(Post::class, PostPolicy::class);
+    }
+
+    protected function tearDown(): void
+    {
+        Mockery::close();
+
+        parent::tearDown();
     }
 
     #[Test, dataProvider('provideData')]
@@ -50,5 +60,44 @@ class VPolicyTest extends TestCase
             ['restore', [$post]],
             ['forceDelete', [$post]],
         ];
+    }
+
+    #[Test]
+    public function guessAbilityWithNoCustomNamespace(): void
+    {
+        $spyUser = Mockery::spy(User::class)->makePartial();
+
+        $mockPolicy = Mockery::mock(PostPolicy::class)->makePartial()->shouldAllowMockingProtectedMethods();
+        $mockPolicy->shouldReceive('getModel')->andReturn('App\Models\Post');
+
+        self::assertFalse($mockPolicy->viewAny($spyUser));
+
+        $spyUser->shouldHaveReceived()->can('post-_mockery_handleMethodCall')->once();
+    }
+
+    #[Test]
+    public function guessAbilityWithCustomNamespace(): void
+    {
+        $spyUser = Mockery::spy(User::class)->makePartial();
+
+        $mockPolicy = Mockery::mock(PostPolicy::class)->makePartial()->shouldAllowMockingProtectedMethods();
+        $mockPolicy->shouldReceive('getModel')->andReturn('App\Models\Blog\Post');
+
+        self::assertFalse($mockPolicy->viewAny($spyUser));
+
+        $spyUser->shouldHaveReceived()->can('blog-post-_mockery_handleMethodCall')->once();
+    }
+
+    #[Test]
+    public function guessAbilityWithUncommonNamespace(): void
+    {
+        $spyUser = Mockery::spy(User::class)->makePartial();
+
+        $mockPolicy = Mockery::mock(PostPolicy::class)->makePartial()->shouldAllowMockingProtectedMethods();
+        $mockPolicy->shouldReceive('getModel')->andReturn('Hans\Valravn\Tests\Core\Models\Post');
+
+        self::assertFalse($mockPolicy->viewAny($spyUser));
+
+        $spyUser->shouldHaveReceived()->can('models-post-_mockery_handleMethodCall')->once();
     }
 }
