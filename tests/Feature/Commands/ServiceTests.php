e882342ab2ba7957dca787e963048175e5bfaa00
Mohammad Mortazavi <s.mohammad027@live.com>
2025-01-20 10:10:24 +0330

    Service refactored;

--- tests/Feature/Commands/ServiceTests.php
+++ tests/Feature/Commands/ServiceTests.php
@@ -3,8 +3,6 @@
 namespace Hans\Valravn\Tests\Feature\Commands;
 
 use Hans\Valravn\Tests\TestCase;
-use Illuminate\Support\Facades\Artisan;
-use Illuminate\Support\Facades\File;
 use PHPUnit\Framework\Attributes\Test;
 
 class ServiceTests extends TestCase
@@ -16,7 +14,14 @@ class ServiceTests extends TestCase
 
         self::assertFileDoesNotExist($crud);
 
-        Artisan::call('valravn:service blog posts');
+        $this->artisan('valravn:service blog posts')
+             ->expectsOutput('CRUD service created.')
+             ->doesntExpectOutput('CRUD service exists or could be created.')
+             ->doesntExpectOutput('Relations service created.')
+             ->doesntExpectOutput('Relations service exists or could be created.')
+             ->doesntExpectOutput('Actions service created.')
+             ->doesntExpectOutput('Actions service exists or could be created.')
+             ->assertSuccessful();
 
         self::assertFileExists($crud);
 
@@ -28,13 +33,48 @@ class ServiceTests extends TestCase
     }
 
     #[Test]
+    public function crudExists(): void
+    {
+        $crud = app_path('Services/Blog/Post/PostCrudService.php');
+
+        self::assertFileDoesNotExist($crud);
+
+        $this->artisan('valravn:service blog posts')
+             ->expectsOutput('CRUD service created.')
+             ->doesntExpectOutput('CRUD service exists or could be created.')
+             ->doesntExpectOutput('Relations service created.')
+             ->doesntExpectOutput('Relations service exists or could be created.')
+             ->doesntExpectOutput('Actions service created.')
+             ->doesntExpectOutput('Actions service exists or could be created.')
+             ->assertSuccessful();
+
+        $this->artisan('valravn:service blog posts')
+             ->doesntExpectOutput('CRUD service created.')
+             ->expectsOutput('CRUD service exists or could be created.')
+             ->doesntExpectOutput('Relations service created.')
+             ->doesntExpectOutput('Relations service exists or could be created.')
+             ->doesntExpectOutput('Actions service created.')
+             ->doesntExpectOutput('Actions service exists or could be created.')
+             ->assertSuccessful();
+
+        self::assertFileExists($crud);
+    }
+
+    #[Test]
     public function relations(): void
     {
         $relations = app_path('Services/Blog/Post/PostRelationsService.php');
 
         self::assertFileDoesNotExist($relations);
 
-        Artisan::call('valravn:service blog posts -r');
+        $this->artisan('valravn:service blog posts -r')
+             ->expectsOutput('CRUD service created.')
+             ->doesntExpectOutput('CRUD service exists or could be created.')
+             ->expectsOutput('Relations service created.')
+             ->doesntExpectOutput('Relations service exists or could be created.')
+             ->doesntExpectOutput('Actions service created.')
+             ->doesntExpectOutput('Actions service exists or could be created.')
+             ->assertSuccessful();
 
         self::assertFileExists($relations);
 
@@ -47,22 +87,83 @@ class ServiceTests extends TestCase
     }
 
     #[Test]
+    public function relationsExists(): void
+    {
+        $relations = app_path('Services/Blog/Post/PostRelationsService.php');
+
+        self::assertFileDoesNotExist($relations);
+
+        $this->artisan('valravn:service blog posts -r')
+             ->expectsOutput('CRUD service created.')
+             ->doesntExpectOutput('CRUD service exists or could be created.')
+             ->expectsOutput('Relations service created.')
+             ->doesntExpectOutput('Relations service exists or could be created.')
+             ->doesntExpectOutput('Actions service created.')
+             ->doesntExpectOutput('Actions service exists or could be created.')
+             ->assertSuccessful();
+
+        $this->artisan('valravn:service blog posts -r')
+             ->doesntExpectOutput('CRUD service created.')
+             ->expectsOutput('CRUD service exists or could be created.')
+             ->doesntExpectOutput('Relations service created.')
+             ->expectsOutput('Relations service exists or could be created.')
+             ->doesntExpectOutput('Actions service created.')
+             ->doesntExpectOutput('Actions service exists or could be created.')
+             ->assertSuccessful();
+
+        self::assertFileExists($relations);
+    }
+
+    #[Test]
     public function actions(): void
     {
         $actions = app_path('Services/Blog/Post/PostActionsService.php');
 
-        File::delete($actions);
         self::assertFileDoesNotExist($actions);
 
-        Artisan::call('valravn:service blog posts -a');
+        $this->artisan('valravn:service blog posts -a')
+             ->expectsOutput('CRUD service created.')
+             ->doesntExpectOutput('CRUD service exists or could be created.')
+             ->doesntExpectOutput('Relations service created.')
+             ->doesntExpectOutput('Relations service exists or could be created.')
+             ->expectsOutput('Actions service created.')
+             ->doesntExpectOutput('Actions service exists or could be created.')
+             ->assertSuccessful();
 
         self::assertFileExists($actions);
 
         $actionsStub = $this->getStub('services/custom.stub');
         $actionsStub = str_replace('{{CRUD-SERVICE::NAMESPACE}}', 'Blog', $actionsStub);
         $actionsStub = str_replace('{{CRUD-SERVICE::MODEL}}', 'Post', $actionsStub);
-        $relationsStub = str_replace('{{CRUD-SERVICE::ACTION}}', 'Actions', $actionsStub);
+        $actionsStub = str_replace('{{CRUD-SERVICE::ACTION}}', 'Actions', $actionsStub);
 
         self::assertEquals($actionsStub, file_get_contents($actions));
+    }
+    #[Test]
+    public function actionsExists(): void
+    {
+        $actions = app_path('Services/Blog/Post/PostActionsService.php');
+
+        self::assertFileDoesNotExist($actions);
+
+        $this->artisan('valravn:service blog posts -a')
+             ->expectsOutput('CRUD service created.')
+             ->doesntExpectOutput('CRUD service exists or could be created.')
+             ->doesntExpectOutput('Relations service created.')
+             ->doesntExpectOutput('Relations service exists or could be created.')
+             ->expectsOutput('Actions service created.')
+             ->doesntExpectOutput('Actions service exists or could be created.')
+             ->assertSuccessful();
+
+        $this->artisan('valravn:service blog posts -a')
+             ->doesntExpectOutput('CRUD service created.')
+             ->expectsOutput('CRUD service exists or could be created.')
+             ->doesntExpectOutput('Relations service created.')
+             ->doesntExpectOutput('Relations service exists or could be created.')
+             ->doesntExpectOutput('Actions service created.')
+             ->expectsOutput('Actions service exists or could be created.')
+             ->assertSuccessful();
+
+        self::assertFileExists($actions);
     }
 }
