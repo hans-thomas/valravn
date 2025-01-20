4a807ea45d8c94f3243dac71030cb90a0199c6b6
Mohammad Mortazavi <s.mohammad027@live.com>
2025-01-20 17:04:46 +0330

    fix;

--- src/Commands/Exception.php
+++ src/Commands/Exception.php
@@ -4,7 +4,6 @@ namespace Hans\Valravn\Commands;
 
 use Hans\Valravn\Commands\Services\ExceptionService;
 use Illuminate\Console\Command;
-use Illuminate\Console\ManuallyFailedException;
 use Symfony\Component\Console\Helper\ProgressBar;
 use Throwable;
 
@@ -33,9 +32,9 @@ class Exception extends Command
     /**
      * Execute the console command.
      *
+     * @return void
      * @throws Throwable
      *
-     * @return void
      */
     public function handle(): int
     {
@@ -50,9 +49,10 @@ class Exception extends Command
         $this->withProgressBar(1, function (ProgressBar $progress) use ($service, $compactName) {
             $this->newLine();
             if ($compactName === '' || filled($compactName) || $this->confirm('Should create a compact exception?')) {
-                $compactName = $compactName ?: $this->ask('What should be its name?');
+                $compactName = $compactName ? : $this->ask('What should be its name?');
                 if (blank($compactName)) {
-                    throw new ManuallyFailedException('The name of the compact exception can not be empty.');
+                    // @phpstan-ignore
+                    $this->fail('The name of the compact exception can not be empty.');
                 }
             }
             if ($compactName) {
