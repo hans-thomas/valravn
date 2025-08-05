3032a6c6a0dfda98b12a424db9f4de490a3449d2
Mohammad Mortazavi <s.mohammad027@live.com>
2025-08-05 18:07:50 +0330

    Update VHandler.php

--- src/Exceptions/VHandler.php
+++ src/Exceptions/VHandler.php
@@ -3,8 +3,10 @@
 namespace Hans\Valravn\Exceptions;
 
 use Exception;
+use Illuminate\Auth\AuthenticationException;
 use Illuminate\Database\QueryException;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Validation\ValidationException;
 use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 use Symfony\Component\HttpKernel\Exception\HttpException;
@@ -23,13 +25,15 @@ class VHandler
         return static fn (Throwable $e) => env('RAW_ERROR', false) ?
             null :
             match (true) {
-                $e instanceof QueryException            => self::throw($e, 9998, $e->getPrevious()->getMessage(), 500),
-                $e instanceof NotFoundHttpException     => self::throw($e, 9997),
+                $e instanceof QueryException => self::throw($e, 9998, $e->getPrevious()->getMessage(), 500),
+                $e instanceof NotFoundHttpException => self::throw($e, 9997),
                 $e instanceof AccessDeniedHttpException => self::throw($e, 9996),
-                $e instanceof BadRequestHttpException   => self::throw($e, 9995),
-                $e instanceof HttpException             => request()->wantsJson() ?
+                $e instanceof BadRequestHttpException => self::throw($e, 9995),
+                $e instanceof ValidationException => self::throw($e, 9994),
+                $e instanceof HttpException => request()->wantsJson() ?
                     self::throw($e, defaultErrorCode: 9994) :
                     null,
+                $e instanceof AuthenticationException => redirect($e->redirectTo(request())),
                 default => self::throw($e)
             };
     }
@@ -37,21 +41,16 @@ class VHandler
     /**
      * Convert the given exception to the ValravnException class.
      *
-     * @param Throwable   $e
-     * @param int         $defaultErrorCode
+     * @param Throwable $e
+     * @param int $defaultErrorCode
      * @param string|null $message
-     * @param int|null    $responseCode
-     *
-     * @throws Exception
+     * @param int|null $responseCode
      *
      * @return JsonResponse
+     * @throws Exception
      */
-    private static function throw(
-        Throwable $e,
-        int $defaultErrorCode = 9999,
-        ?string $message = null,
-        ?int $responseCode = null
-    ): JsonResponse {
+    private static function throw(Throwable $e, int $defaultErrorCode = 9999, ?string $message = null, ?int $responseCode = null): JsonResponse
+    {
         if (method_exists($e, $method = 'getErrorCode')) {
             $errorCode = $e->{$method}();
         } elseif ($e->getCode() > 0) {
@@ -60,8 +59,10 @@ class VHandler
             $errorCode = $defaultErrorCode;
         }
 
-        if ($responseCode == null && method_exists($e, 'getStatusCode')) {
+        if ($responseCode === null && method_exists($e, 'getStatusCode')) {
             $responseCode = $e->getStatusCode();
+        } elseif ($responseCode === null && property_exists($e, 'status')) {
+            $responseCode = $e->status;
         } else {
             $responseCode = 500;
         }
