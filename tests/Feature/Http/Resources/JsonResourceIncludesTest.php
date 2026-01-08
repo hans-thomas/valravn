d6d67a21bdc9afec94cf18471593f6df9cbdc96c
Mohammad Mortazavi <s.mohammad027@live.com>
2026-01-08 12:25:09 +0330

    Add tests for includes on collection resource;

--- tests/Feature/Http/Resources/JsonResourceIncludesTest.php
+++ tests/Feature/Http/Resources/JsonResourceIncludesTest.php
@@ -6,6 +6,7 @@ use Hans\Valravn\Tests\Core\Factories\CommentFactory;
 use Hans\Valravn\Tests\Core\Factories\PostFactory;
 use Hans\Valravn\Tests\Core\Models\Comment;
 use Hans\Valravn\Tests\Core\Models\Post;
+use Hans\Valravn\Tests\Core\Resources\Post\PostCollection;
 use Hans\Valravn\Tests\Core\Resources\Post\PostResource;
 use Hans\Valravn\Tests\TestCase;
 use PHPUnit\Framework\Attributes\Test;
@@ -21,22 +22,22 @@ class JsonResourceIncludesTest extends TestCase
     }
 
     #[Test]
-    public function includes(): void
+    public function includesOnResource(): void
     {
         $resource = PostResource::make($this->post)->withCommentsIncludes();
         self::assertEquals(
             [
                 'data' => [
-                    'type'     => 'posts',
-                    'id'       => $this->post->id,
-                    'title'    => $this->post->title,
-                    'content'  => $this->post->content,
+                    'type' => 'posts',
+                    'id' => $this->post->id,
+                    'title' => $this->post->title,
+                    'content' => $this->post->content,
                     'comments' => $this->post
                         ->comments
                         ->map(
-                            fn (Comment $value) => [
-                                'type'    => 'comments',
-                                'id'      => $value->id,
+                            fn(Comment $value) => [
+                                'type' => 'comments',
+                                'id' => $value->id,
                                 'content' => $value->content,
                             ]
                         )
@@ -49,23 +50,59 @@ class JsonResourceIncludesTest extends TestCase
     }
 
     #[Test]
+    public function includesOnCollection(): void
+    {
+        PostFactory::new()->has(CommentFactory::new()->count(5))->createMany();
+
+        $collection = PostCollection::make(Post::query()->paginate())->withFirstCommentQuery();
+        self::assertEquals(
+            [
+                'data' => Post::query()
+                    ->get()
+                    ->map(
+                        fn(Post $post) => [
+                            'type' => 'posts',
+                            'id' => $post->id,
+                            'title' => $post->title,
+                            'content' => $post->content,
+                            'first_comment' => $post
+                                ->comments
+                                ->map(
+                                    fn(Comment $value) => [
+                                        'type' => 'comments',
+                                        'id' => $value->id,
+                                        'content' => $value->content,
+                                    ]
+                                )
+                                ->toArray()[0] ?? null,
+                        ]
+                    )
+                ->toArray(),
+            ],
+            [
+                'data' => $this->resourceToJson($collection)['data']
+            ]
+        );
+    }
+
+    #[Test]
     public function includesThroughApi(): void
     {
         $content = $this->get("/includes/posts/{$this->post->id}?includes=comments")
-                        ->json();
+            ->json();
         self::assertEquals(
             [
                 'data' => [
-                    'type'     => 'posts',
-                    'id'       => $this->post->id,
-                    'title'    => $this->post->title,
-                    'content'  => $this->post->content,
+                    'type' => 'posts',
+                    'id' => $this->post->id,
+                    'title' => $this->post->title,
+                    'content' => $this->post->content,
                     'comments' => $this->post
                         ->comments
                         ->map(
-                            fn (Comment $value) => [
-                                'type'    => 'comments',
-                                'id'      => $value->id,
+                            fn(Comment $value) => [
+                                'type' => 'comments',
+                                'id' => $value->id,
                                 'content' => $value->content,
                             ]
                         )
